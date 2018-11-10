52e9fd6190fbbe9e32f4b11849cedbab0029aeb9
SorenKremer <skremer01@hamline.edu>
2018-11-10 16:01:13 -0600

    heres Practice code

---
+++ Practice.py
@@ -0,0 +1,19 @@
+print( "hello world")
+x=5
+y=7
+print (x + y)
+"""comment"""
+def addition (a, b):
+    """Returns the addition of two numbers"""
+    return a + b
+
+
+
+
+print (addition(x,y))
+
+import random
+for x in range(1):
+  q = (random.randint(1,10))
+
+  print(q)
