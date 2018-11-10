2c8260300fb9d1dc15361aa1eb6d1609947faa1d
Drew Mares <mikkelmail98@gmail.com>
2018-11-10 13:20:28 -0600

    Added first ever python file

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# File: Hello.py
+
+print ("Hello World!")
+print("This is my first python program")
+print("x=2   y=3   z=x+y")
+x=2
+y=3
+z=x+y
+print(z)
+
+for i in range(0, 20):
+    print(i)
+    if i==12 or i==10:
+        break
