7dbbe9fe3864036bda2e45b3848e58d95b27607f
Drew Mares <mikkelmail98@gmail.com>
2018-11-11 13:54:57 -0600

    Streamlined the code as well as fixing ending screen bug

--- Color_Game.py
+++ Color_Game.py
@@ -38,99 +38,61 @@ while number_of_trials < 16:
     random_number = random_number % 10
 
     if random_number == 0:
-
         b.screen.lcd.text("Up", 40, 20)
-
     if random_number == 1:
         b.screen.lcd.text("Right", 40, 20)
-
     if random_number == 2:
         b.screen.lcd.text("Down", 40, 20)
-
     if random_number == 3:
         b.screen.lcd.text("Left", 40, 20)
-
     if random_number == 4:
         b.screen.lcd.text("Red", 40, 20)
-
     if random_number == 5:
         b.screen.lcd.text("B", 40, 20)
-
     if random_number == 6:
         b.screen.lcd.text("A", 40, 20)
-
     if random_number == 7:
         b.screen.lcd.text("Orange", 40, 20)
-
     if random_number == 8:
         b.screen.lcd.text("Green", 40, 20)
-
     if random_number == 9:
         b.screen.lcd.text("Blue", 40, 20)
-
     b.screen.lcd.show()
-
     time.sleep(1)
     user_input = b.irq.get_pressed_button()
     if user_input is None:
         user_input = 100
     if random_number == 0:
-        if user_input == 0:
-            """do nothing"""
-        else:
+        if user_input != 0:
             wrong_press = wrong_press + 1
-
     if random_number == 1:
-        if user_input == 1:
-            """do nothing"""
-        else:
+        if user_input != 1:
             wrong_press = wrong_press + 1
-
     if random_number == 2:
-        if user_input == 2:
-            """do nothing"""
-        else:
+        if user_input != 2:
             wrong_press = wrong_press + 1
-
     if random_number == 3:
-        if user_input == 3:
-            """do nothing"""
-        else:
+        if user_input != 3:
             wrong_press = wrong_press + 1
-
     if random_number == 4:
-        if user_input == 5:
-            """do nothing"""
-        else:
+        if user_input != 5:
             wrong_press = wrong_press + 1
-
     if random_number == 5:
-        if user_input == 5:
-            """do nothing"""
-        else:
+        if user_input != 5:
             wrong_press = wrong_press + 1
-
     if random_number == 6:
-        if user_input == 6:
-            """do nothing"""
-        else:
+        if user_input != 6:
             wrong_press = wrong_press + 1
-
     if random_number == 7:
         if user_input == 0:
-
             """do nothing"""
         elif user_input == 2:
             """do nothing"""
         else:
             wrong_press = wrong_press + 1
-
     if random_number == 8:
-        if user_input == 6:
-            """do nothing"""
-        else:
+        if user_input != 6:
             wrong_press = wrong_press + 1
-
     if random_number == 9:
         if user_input == 1:
             """do nothing"""
@@ -138,15 +100,15 @@ while number_of_trials < 16:
             """do nothing"""
         else:
             wrong_press = wrong_press + 1
-
     b.screen.lcd.fill(0)
     b.screen.show()
     time.sleep(.5)
     number_of_trials = number_of_trials + 1
-
+    b.irq.clear_buffer()
 time_end = time.time()
 time_score = (time_end - time_start)
 button = None
+b.irq.clear_buffer()
 while button is None:
     b.screen.lcd.fill(0)
     b.screen.lcd.text("Your score is ", 0, 0)
