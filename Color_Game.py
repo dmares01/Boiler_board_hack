23d4671ddb6627b8e73bb8ed804bc93bb87671ca
Drew Mares <mikkelmail98@gmail.com>
2018-11-11 14:13:31 -0600

    added working timer

--- Color_Game.py
+++ Color_Game.py
@@ -23,9 +23,9 @@ while button_press is None:
 
 b.screen.lcd.fill(0)
 urandom.seed(time.time())
-time_start = time.time()
 wrong_press = 0
 number_of_trials = 1
+time_score = 0
 while number_of_trials < 16:
     b.screen.lcd.text("Round", 20, 0)
     b.screen.lcd.text(str(number_of_trials), 25, 10)
@@ -36,7 +36,7 @@ while number_of_trials < 16:
     time.sleep(.5)
     random_number = urandom.getrandbits(10)
     random_number = random_number % 10
-
+    time_start = time.time()
     if random_number == 0:
         b.screen.lcd.text("Up", 40, 20)
     if random_number == 1:
@@ -92,13 +92,13 @@ while number_of_trials < 16:
     if random_number == 9:
         if user_input != 1 and user_input != 3:
             wrong_press = wrong_press + 1
+    time_end = time.time()
+    time_score = time_score + (time_end - time_start)
     b.screen.lcd.fill(0)
     b.screen.show()
     time.sleep(.5)
     number_of_trials = number_of_trials + 1
     b.irq.clear_buffer()
-time_end = time.time()
-time_score = (time_end - time_start)
 button = None
 b.irq.clear_buffer()
 while button is None:
